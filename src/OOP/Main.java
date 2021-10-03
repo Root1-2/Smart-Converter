7bbf7c01fe66be80ef47ef196c9398ac9c87cba6
rafi-10 <jafulhaque99@yahoo.com>
2021-10-04 01:49:29 +0600

    a print statement added in main class.

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -3,6 +3,14 @@ package OOP;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("/**************************\n" +
+                            "*    Hello Good People    *\n" +
+                           "***************************/\n\n");
+
+        System.out.println("/-----------------------------\n" +
+                           "+  Keep attention here       +\n" +
+                           "+  A Stunning coming soon... +\n" +
+                           "------------------------------/");
     }
 }
