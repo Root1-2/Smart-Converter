b0376e6a117217ab7f5668d0f3d07aa348a67d4e
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2021-10-30 21:44:41 +0600

    Update aboutUs.java

--- src/OOP/aboutUs.java
+++ src/OOP/aboutUs.java
@@ -7,41 +7,41 @@ public class aboutUs extends Main
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\t\t\t\t\t\t\t\t***ABOUT OUR PROJECT***\n"+
-                "\t\t\t\t\t\t\t\t  [ SMART CONVERTER ]\n\n\n\nIN EARLIER YEARS"+
+        System.out.println("\n\n\n\t\t\t\t\t\t\t\t\t\t***ABOUT OUR PROJECT***\n"+
+                "\t\t\t\t\t\t\t\t\t\t  [ SMART CONVERTER ]\n\n\n\n\t\t\t\t\t\t\tIN EARLIER YEARS"+
                 "ALL THE CONVERSION OF DIFFERENT SUBJECT HAD TO BE DONE MANUALLY\n"+
-                "PEOPLE HAD TO REMEMBER LOTS OF FORMULA TO CHANGE ONE UNITS TO ANOTHER.\n"+
-                "BUT WITH OUR PROJECT, PEOPLE CAN EASILY MAKE THIS CHANGE WITH THEIR FINGERTIP\n"+
-                "ONLY, WITHOUT ANY MISTAKES & FASTER THAN EVER. IT HAS FEATURES LIKE TIME CONVERSION,\n"+
-                "AREA CONVERSION, CURRENCY CONVERSION, TEMPERATURE CONVERSION WHICH IS SOMETHING WE ALL\n"+
-                "NEED SOMETIME IN OUR DAY-TO-DAY LIFE. THE WHOLE PROJECT IS DEVELOPED IN JAVA PROGRAMMING\n"+
-                "LANGUAGE. MANY FUNCTIONS, DIFFERENT VARIABLES, STRINGS & CLASS HAS BEEN USED\n"+
-                "FOR THE DEVELOPMENT OF THIS PROJECT. THIS IS A MINI PROJECT SO IT CONTAINS LESS FEATURES\n"+
-                "THAN USUAL. BUT IT'S EASY TO OPERATE AND UNDERSTANDABLE BY THE USERS AND HAVE THE\n"+
-                "OPTIONS TO ADD TONS OF FEATURES IN THE UPCOMING UPDATES\n");
+                "\t\t\t\t\t\t\tPEOPLE HAD TO REMEMBER LOTS OF FORMULA TO CHANGE ONE UNITS TO ANOTHER.\n"+
+                "\t\t\t\t\t\t\tBUT WITH OUR PROJECT, PEOPLE CAN EASILY MAKE THIS CHANGE WITH THEIR FINGERTIP\n"+
+                "\t\t\t\t\t\t\tONLY, WITHOUT ANY MISTAKES & FASTER THAN EVER. IT HAS FEATURES LIKE TIME CONVERSION,\n"+
+                "\t\t\t\t\t\t\tAREA CONVERSION, CURRENCY CONVERSION, TEMPERATURE CONVERSION WHICH IS SOMETHING WE ALL\n"+
+                "\t\t\t\t\t\t\tNEED SOMETIME IN OUR DAY-TO-DAY LIFE. THE WHOLE PROJECT IS DEVELOPED IN JAVA PROGRAMMING\n"+
+                "\t\t\t\t\t\t\tLANGUAGE. MANY FUNCTIONS, DIFFERENT VARIABLES, STRINGS & CLASS HAS BEEN USED\n"+
+                "\t\t\t\t\t\t\tFOR THE DEVELOPMENT OF THIS PROJECT. THIS IS A MINI PROJECT SO IT CONTAINS LESS FEATURES\n"+
+                "\t\t\t\t\t\t\tTHAN USUAL. BUT IT'S EASY TO OPERATE AND UNDERSTANDABLE BY THE USERS AND HAVE THE\n"+
+                "\t\t\t\t\t\t\tOPTIONS TO ADD TONS OF FEATURES IN THE UPCOMING UPDATES\n\n\n");
 
-        System.out.println("\t\t\t\t\t\t\t\t\t** ABOUT US**\n\n\n\n"+
-                "\t\tA GROUP OF FIVE MEMBERS WORKING TOGETHER FROM PLACES "+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t*** ABOUT US***\n\n\n\n"+
+                "\t\t\t\tA GROUP OF FIVE MEMBERS WORKING TOGETHER FROM PLACES "+
                 "BATCHES OF DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING (LEADING UNIVERSITY)\n");
 
-        System.out.println("\n\n\n\t\t\t\t\t\t\t< 1 > MD. JAFRUL HAQUE RAFI         2012020259\n"+
+        System.out.println("\n\t\t\t\t\t\t\t< 1 > MD. JAFRUL HAQUE RAFI         2012020259\n"+
                 "\t\t\t\t\t\t\t< 2 > TASFIAH TAHSIN AHSAN          2012020258\n"+
                 "\t\t\t\t\t\t\t< 3 > ABDULLAH AL MASRUR            2012020255\n"+
                 "\t\t\t\t\t\t\t< 4 > ANISUR RAHMAN RIFAT           2012020229\n"+
                 "\t\t\t\t\t\t\t< 5 > JANNATUL FERDOUS SABRIN       2012020251\n");
 
-
-        System.out.println("FOR ANY KIND OF HELP YOU CAN CONTRACT WITH US USING BELOW INFORMATION:\n\n"+
-                "MAIL:\ncse_2012020259@lus.ac.bd\n" +
-                        "cse_2012020255@lus.ac.bd\n" +
-                        "cse_2012020251@lus.ac.bd\n" +
-                        "cse_2012020258@lus.ac.bd\n" +
-                        "cse_2012020229@lus.ac.bd\n\n" +
-                        "MOBILE : \n" +
-                        "01797224414"
+        System.out.println("\n\t\t\t\t\t\t\t\t\t\t*** CONTACT INFO***\n");
+        System.out.println("\t\t\t\t\t\t\tFOR ANY KIND OF HELP YOU CAN CONTRACT WITH US USING BELOW INFORMATION:\n\n"+
+                "\t\t\t\t\t\t\tMAIL:\n\t\t\t\t\t\t\tcse_2012020259@lus.ac.bd\n" +
+                        "\t\t\t\t\t\t\tcse_2012020255@lus.ac.bd\n" +
+                        "\t\t\t\t\t\t\tcse_2012020251@lus.ac.bd\n" +
+                        "\t\t\t\t\t\t\tcse_2012020258@lus.ac.bd\n" +
+                        "\t\t\t\t\t\t\tcse_2012020229@lus.ac.bd\n\n" +
+                        "\t\t\t\t\t\t\tMOBILE : \n" +
+                        "\t\t\t\t\t\t\t01797224414"
                 );
 
-        System.out.println("\nWOULD YOU LIKE TO GO BACK TO MAIN MENU ");
+        System.out.println("\nWOULD YOU LIKE  BACK TO MAIN MENU ");
         System.out.println("< 1 > YES");
         System.out.println("< 2 > NO");
 
