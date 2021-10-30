6169350be7461ba2e6d5c70be5e2d49782c2e2e1
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2021-10-30 20:26:29 +0600

    Update temperatureConversion.java

--- src/OOP/temperatureConversion.java
+++ src/OOP/temperatureConversion.java
@@ -8,14 +8,14 @@ public class temperatureConversion
     {
         Scanner in = new Scanner(System.in);
         System.out.println("\n\t\tMENU:\n\t\t=====");
-        System.out.println("< 1 > Celsius to Fahrenheit");
-        System.out.println("< 2 > Fahrenheit to Celsius");
-        System.out.println("< 3 > Celsius to Kelvin");
-        System.out.println("< 4 > Kelvin to Celsius");
-        System.out.println("< 5 > Fahrenheit to Kelvin");
-        System.out.println("< 6 > Kelvin to Fahrenheit\n");
+        System.out.println("< 1 > CELSIUS -> FAHRENHEIT");
+        System.out.println("< 2 > FAHRENHEIT -> CELSIUS");
+        System.out.println("< 3 > CELSIUS -> KELVIN");
+        System.out.println("< 4 > KELVIN -> CELSIUS");
+        System.out.println("< 5 > FAHRENHEIT -> KELVIN");
+        System.out.println("< 6 > KELVIN -> FAHRENHEITt\n");
 
-        System.out.println("Please enter any of the above option: ");
+        System.out.println("PLEASE ENTER ANY OF THE OPTIONS ABOVE: ");
 
         int ch = in.nextInt();
         double ft, ct, kt;
@@ -23,45 +23,45 @@ public class temperatureConversion
         switch (ch)
         {
             case 1:
-                System.out.print("Enter Temperature in Celsius: ");
+                System.out.print("ENTER TEMPERATURE IN CELSIUS: ");
                 ct = in.nextDouble();
                 ft = 9.0 / 5.0 * ct + 32;
-                System.out.printf("Temperature in Fahrenheit: %.2f", ft);
+                System.out.printf("TEMPERATURE IN FAHRENHEIT: %.2f", ft);
                 break;
 
             case 2:
-                System.out.print("Enter Temperature in Fahrenheit: ");
+                System.out.print("ENTER TEMPERATURE IN FAHRENHEIT: ");
                 ft = in.nextDouble();
                 ct = (ft - 32) * 5 / 9.0;
-                System.out.printf("Temperature in Celsius: %.2f", ct);
+                System.out.printf("TEMPERATURE IN CELSIUS: %.2f", ct);
                 break;
 
             case 3:
-                System.out.print("Enter Temperature in Celsius : ");
+                System.out.print("ENTER TEMPERATURE IN CELSIUS : ");
                 ct = in.nextDouble();
                 kt = ct + 273.15;
-                System.out.printf("Temperature in Kelvin : %.2f", kt);
+                System.out.printf("TEMPERATURE IN KELVIN : %.2f", kt);
                 break;
 
             case 4:
-                System.out.print("Enter Temperature in Kelvin : ");
+                System.out.print("ENTER TEMPERATURE IN KELVIN : ");
                 kt = in.nextDouble();
                 ct = kt - 273.15;
-                System.out.printf("Temperature in Celsius : %.2f", ct);
+                System.out.printf("ENTER TEMPERATURE IN CELSIUS : %.2f", ct);
                 break;
 
             case 5:
-                System.out.printf("Enter Temperature in Fahrenheit : ");
+                System.out.printf("ENTER TEMPERATURE IN FAHRENHEIT : ");
                 ft = in.nextDouble();
                 kt = ((ft-32)/1.8) + 273.15;
-                System.out.printf("Temperature in Kelvin : $.2f", kt);
+                System.out.printf("TEMPERATURE IN KELVIN : $.2f", kt);
                 break;
 
             case 6:
-                System.out.printf("Enter Temperature in Kelvin : ");
+                System.out.printf("ENTER TEMPERATURE IN KELVIN : ");
                 kt = in.nextDouble();
                 ft = (kt - 273.15) * 9/5 + 32;
-                System.out.printf("Temperature in Fahrenheit : %.2f", ft);
+                System.out.printf("TEMPERATURE IN FAHRENHEIT : %.2f", ft);
                 break;
 
             default:
