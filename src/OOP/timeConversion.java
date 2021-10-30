3e1f85f271e592668f78f9e849b8d5cdb2b9072e
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2021-10-30 20:31:45 +0600

    Update timeConversion.java

--- src/OOP/timeConversion.java
+++ src/OOP/timeConversion.java
@@ -10,87 +10,90 @@ public class timeConversion
         Scanner sc = new Scanner(System.in);
 
         System.out.println("\n\t\tMENU:\n\t\t=====");
-        System.out.println("1.Current Time.");
-        System.out.println("2.Bangladesh -> London.");
-        System.out.println("3.Bangladesh -> America.");
-        System.out.println("4.Bangladesh -> Canada.");
-        System.out.println("5.Bangladesh -> Italy.");
+        System.out.println("1.CURRENT TIME.");
+        System.out.println("2.BANGLADESH -> LONDON.");
+        System.out.println("3.BANGLADESH -> AMERICA.");
+        System.out.println("4.BANGLADESH -> CANADA.");
+        System.out.println("5.BANGLADESH -> ITALY.");
 
         System.out.println("\n");
-        System.out.println("Please enter any of the above option: ");
+        System.out.println("PLEASE ENTER ANY OF THE OPTIONS ABOVE: ");
 
         int ch = sc.nextInt();
 
-        switch(ch) {
+        switch(ch) 
+        {
             case 1:
                 Date date=java.util.Calendar.getInstance().getTime();
                 System.out.println(date);
                 break;
+                
             case 2:
-                System.out.print("Enter Hour ");
+                System.out.print("ENTER HOUR ");
                 int h1 = sc.nextInt();
-                System.out.print("Enter Minutes ");
+                System.out.print("ENTER MINUTES ");
                 int m1 = sc.nextInt();
-                System.out.println("Time Is: "+h1+":"+ m1);
+                System.out.println("BANGLADESHI TIME IS: "+h1+":"+ m1);
 
                 int londonHour = h1 - 5;
 
-                System.out.println("London time :"+ londonHour+":"+ m1);
+                System.out.println("LONDON TIME IS :"+ londonHour+":"+ m1);
                 break;
 
 
             case 3:
-                System.out.print("Enter Hour(Internation Format) ");
+                System.out.print("ENTER HOUR ");
                 int h2 = sc.nextInt();
-                System.out.print("Enter Minutes ");
+                System.out.print("ENTER MINUTES ");
                 int m2 = sc.nextInt();
-                System.out.println("Time Is: "+h2+":"+ m2);
+                System.out.println("BANGLADESHI TIME IS: "+h2+":"+ m2);
 
                 int AmericaHour = h2 + 14;
                 if(AmericaHour >= 24)
                 {
                 AmericaHour = AmericaHour-24;
-                System.out.println("America time :"+ AmericaHour+":"+ m2);
+                System.out.println("AMERICAN TIME IS :"+ AmericaHour+":"+ m2);
                 }
                 else
                 {
-                    System.out.println("America time :"+ AmericaHour+":"+ m2);
+                    System.out.println("AMERICAN TIME IS :"+ AmericaHour+":"+ m2);
                 }
+                
             case 4:
-                System.out.print("Enter Hour");
+                System.out.print("ENTER HOUR ");
                 int h3 = sc.nextInt();
-                System.out.print("Enter Minutes ");
+                System.out.print("ENTER MINUTES ");
                 int m3 = sc.nextInt();
-                System.out.println("Time Is: "+h3+":"+ m3);
+                System.out.println("BANGLADESHI TIME IS: "+h3+":"+ m3);
 
                 int CanadaHour = h3 + 14;
                 if(CanadaHour >= 24)
                 {
                     CanadaHour = CanadaHour-24;
-                    System.out.println("America time :"+ CanadaHour+":"+ m3);
+                    System.out.println("CANADIAN TIME IS :"+ CanadaHour+":"+ m3);
                 }
                 else
                 {
-                    System.out.println("America time :"+ CanadaHour+":"+ m3);
+                    System.out.println("CANADIAN TIME IS :"+ CanadaHour+":"+ m3);
                 }
                 break;
 
             case 5:
-                System.out.print("Enter Hour");
+                System.out.print("ENTER HOUR ");
                 int h4 = sc.nextInt();
-                System.out.print("Enter Minutes ");
+                System.out.print("ENTER MINUTES ");
                 int m4 = sc.nextInt();
-                System.out.println("Time Is: "+h4+":"+ m4);
+                System.out.println("BANGLADESHI TIME IS: "+h4+":"+ m4);
 
                 int ItalyHour = h4 + 20;
                 if(ItalyHour >= 24)
                 {
                     ItalyHour = ItalyHour-24;
-                    System.out.println("Italy time :"+ ItalyHour+":"+ m4);
+                    System.out.println("ITALIAN TIME IS:"+ ItalyHour+":"+ m4);
                 }
                 else
                 {
-                    System.out.println("Italy time :"+ ItalyHour+":"+ m4);
+                    System.out.println("ITALIAN TIME IS:"+ ItalyHour+":"+ m4);
                 }
                 break;
 
