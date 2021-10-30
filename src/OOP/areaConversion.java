6c1aa879bf382ef1f0b17cae628bff0fab54d250
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2021-10-30 20:51:38 +0600

    Update areaConversion.java

--- src/OOP/areaConversion.java
+++ src/OOP/areaConversion.java
@@ -8,72 +8,72 @@ public class areaConversion {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("\n\t\tMENU:\n\t\t=====");
-        System.out.println("< 1 > Area of Circle");
-        System.out.println("< 2 > Area of Triangle");
-        System.out.println("< 3 > Area of Rectangle");
-        System.out.println("< 4 > Area of Square");
-        System.out.println("< 5 > Area of Trapezium");
-        System.out.println("< 6 > Area of Rhombus");
-        System.out.println("Please enter any of the above option: ");
+        System.out.println("< 1 > AREA OF CIRCLE");
+        System.out.println("< 2 > AREA OF TRIANGLE");
+        System.out.println("< 3 > AREA OF RECTANGLE");
+        System.out.println("< 4 > AREA OF SQUARE");
+        System.out.println("< 5 > AREA OF TRAPEZIUM");
+        System.out.println("< 6 > AREA OF RHOMBUS");
+        System.out.println("PLEASE ENTER ANY OF THE OPTIONS ABOVE: ");
 
         int ch = sc.nextInt();
 
         switch(ch)
         {
             case 1:
-                System.out.println("Please enter radius of circle: ");
+                System.out.println("ENTER RADIUS: ");
                 double radius = sc.nextFloat();
                 double areaCircle = (22 * radius * radius) / 7;
                 System.out.println("Area of circle is: " + areaCircle);
                 break;
             case 2:
-                System.out.println("Please enter base of triangle: ");
+                System.out.println("ENTER BASE: ");
                 double base = sc.nextFloat();
-                System.out.println("Please enter height of triangle: ");
+                System.out.println("\nENTER HEIGHT: ");
                 double height = sc.nextFloat();
                 double areaTriangle = (base * height) / 2;
-                System.out.println("Area of triangle is: " + areaTriangle);
+                System.out.println("AREA OF TRIANGLE IS: " + areaTriangle);
                 break;
 
             case 3:
-                System.out.println("Please enter length of rectangle: ");
+                System.out.println("ENTER LENGTH: ");
                 int length = sc.nextInt();
-                System.out.println("Please enter breadth of rectangle: ");
+                System.out.println("ENTER BREADTH: ");
                 int breadth = sc.nextInt();
                 int areaRectangle = length * breadth;
-                System.out.println("Area of rectangle is: " + areaRectangle);
+                System.out.println("AREA OF RECTANGLE IS: " + areaRectangle);
                 break;
 
             case 4:
-                System.out.println("Please enter any side of Square: ");
+                System.out.println("ENTER ANY ONE SIDE: ");
                 int a = sc.nextInt();
                 int square = a * a;
-                System.out.println("Area of Square is: " + square);
+                System.out.println("AREA OF SQUARE IS: " + square);
                 break;
 
             case 5:
-                System.out.println("Please enter base1 of trapezium: ");
+                System.out.println("ENTER BASE1: ");
                 double base1 = sc.nextFloat();
-                System.out.println("Please enter base2 of trapezium: ");
+                System.out.println("ENTER BASE2: ");
                 double base2 = sc.nextFloat();
-                System.out.println("Please enter height of trapezium: ");
+                System.out.println("ENTER HEIGHT: ");
                 double h = sc.nextFloat();
                 double areaTrapezium = ((base1 + base2) * h)/2;
-                System.out.println("Area of triangle is: " + areaTrapezium);
+                System.out.println("AREA OF TRAPEZIUM: " + areaTrapezium);
                 break;
 
             case 6:
-                System.out.println("Please enter diagonal1: ");
+                System.out.println("ENTER DIAGONAL1: ");
                 double d1 = sc.nextFloat();
-                System.out.println("Please enter diagonal2: ");
+                System.out.println("ENTER DIAGONAL2: ");
                 double d2 = sc.nextFloat();
                 double areaRhombus = (d1 * d2) / 2;
-                System.out.println("Area of Rhombus is: " + areaRhombus);
+                System.out.println("AREA OF RHOMBUS: " + areaRhombus);
                 break;
 
 
             default:
-                System.out.println("Incorrect Choice");
+                System.out.println("INCORRECT CHOICE");
                 System.out.println("WOULD YOU LIKE TO TRY AGAIN");
                 System.out.println("< 1 > YES");
                 System.out.println("< 2 > NO");
