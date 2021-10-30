f84ed2241a8274dfea66a1035b83b56cb38d7be7
Md. Jafrul Haque Rafi <jafrulhaque99@yahoo.com>
2021-10-31 01:00:21 +0600

    Update currencyConversion.java

--- src/OOP/currencyConversion.java
+++ src/OOP/currencyConversion.java
@@ -1,23 +1,23 @@
 package OOP;
 import java.util.Scanner;
 
-public class currencyConversion
+public class currencyConversion extends Main
 {
     public static void display()
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\n\t\tMENU:\n\t\t=====");
-        System.out.println("< 1 > TK -> DOLLAR ");
-        System.out.println("< 2 > TK -> EURO");
-        System.out.println("< 3 > TK -> RUPEE");
-        System.out.println("< 4 > TK -> CANADIAN DOLLAR");
-        System.out.println("< 5 > TK -> BRAZILIAN REAL");
-        System.out.println("< 6 > TK -> SAUDIAN REAL");
-        System.out.println("< 7 > TK -> ARGENTINE PESO");
-        System.out.println("< 8 > TK -> AUSTRALIAN DOLLAR");
-        System.out.println("< 9 > TK -> QATARI RIYAL");
-        System.out.println("< 10 > TK -> OMANI RIAL\n");
+        System.out.println("\n\t\tCURRENCY CONVERSION:\n\t\t====================");
+        System.out.println("< 1 >  DOLLAR -> TK ");
+        System.out.println("< 2 >  EURO -> TK");
+        System.out.println("< 3 >  RUPEE -> TK");
+        System.out.println("< 4 >  CANADIAN DOLLAR -> TK");
+        System.out.println("< 5 >  BRAZILIAN REAL -> TK");
+        System.out.println("< 6 >  SAUDIAN REAL -> TK ");
+        System.out.println("< 7 >  ARGENTINE PESO -> TK");
+        System.out.println("< 8 >  AUSTRALIAN DOLLAR -> TK");
+        System.out.println("< 9 >  QATARI RIYAL -> TK");
+        System.out.println("< 10 > OMANI RIAL -> TK\n");
 
 
         System.out.println("PLEASE ENTER ANY OF THE OPTIONS ABOVE: ");
@@ -27,82 +27,82 @@ public class currencyConversion
         switch (ch)
         {
             case 1:
-                System.out.println("ENTER TK: ");
+                System.out.println("ENTER DOLLAR : ");
                 float x1 = sc.nextFloat();
-                double y1 = (x1/85.61);
+                double y1 = (x1 * 85.61);
 
-                System.out.printf("YOUR CURRENCY IN DOLLAR IS: %.2f$\n", y1);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y1);
                 break;
 
             case 2:
-                System.out.println("ENTER TK: ");
+                System.out.println("ENTER EURO: ");
                 float x2 = sc.nextFloat();
 
-                double y2 = (x2/99.42);
-                System.out.printf("YOUR CURRENCY IN EURO IS: %.2f€\n", y2);
+                double y2 = (x2 * 99.42);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y2);
                 break;
 
             case 3:
-                System.out.println("ENTER TK: ");
+                System.out.println("ENTER RUPEE: ");
                 float x3 = sc.nextFloat();
-                double y3 = (x3/1.14);
+                double y3 = (x3*1.14);
 
-                System.out.printf("YOUR CURRENCY IN RUPEE IS: %.2f₹\n", y3);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y3);
                 break;
 
             case 4:
-                System.out.println("ENTER TK: ");
+                System.out.println("ENTER CANADIAN DOLLAR: ");
                 float x4 = sc.nextFloat();
-                double y4 = (x4/69.3);
+                double y4 = (x4*69.3);
 
-                System.out.printf("YOUR CURRENCY IN CANADIAN DOLLAR IS: %.2f$\n", y4);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y4);
                 break;
 
             case 5:
-                System.out.println("ENTER TK: ");
+                System.out.println("BRAZILIAN REAL: ");
                 float x5 = sc.nextFloat();
-                double y5 = (x5/15.36);
+                double y5 = (x5*15.36);
 
-                System.out.printf("YOUR CURRENCY IN BRAZILIAN REAL IS: %.2fR$\n", y5);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y5);
                 break;
 
             case 6:
-                System.out.println("ENTER TK: ");
+                System.out.println("SAUDIAN REAL: ");
                 float x6 = sc.nextFloat();
-                double y6 = (x6/22.83);
+                double y6 = (x6*22.83);
 
-                System.out.printf("YOUR CURRENCY IN SAUDIAN REAL IS : %.2fSAR\n", y6);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y6);
                 break;
 
             case 7:
-                System.out.println("ENTER TK: ");
+                System.out.println("ARGENTINE PESO: ");
                 float x7 = sc.nextFloat();
-                double y7 = (x7/64.41);
+                double y7 = (x7*64.41);
 
-                System.out.printf("YOUR CURRENCY IN ARGENTINE PESO IS: %.2fARS\n", y7);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y7);
                 break;
 
             case 8:
-                System.out.println("ENTER TK: ");
+                System.out.println("AUSTRALIAN DOLLAR: ");
                 float x8 = sc.nextFloat();
-                double y8 = (x8/64.42);
-                System.out.printf("YOUR CURRENCY IN AUSTRALIAN DOLLAR IS: %.2fAU$\n", y8);
+                double y8 = (x8*64.42);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y8);
                 break;
 
             case 9:
-                System.out.println("ENTER TK: ");
+                System.out.println("QATARI RIYAL: ");
                 float x9 = sc.nextFloat();
-                double y9 = (x9/23.52);
+                double y9 = (x9*23.52);
 
-                System.out.printf("YOUR CURRENCY IN QATARI RIYAL IS: %.2fQAR\n", y9);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y9);
                 break;
 
             case 10:
-                System.out.println("ENTER TK: ");
+                System.out.println("OMANI RIAL: ");
                 float x10 = sc.nextFloat();
-                double y10 = (x10/222.444);
+                double y10 = (x10*222.444);
 
-                System.out.printf("YOUR CURRENCY IN OMANI REAL IS: %.2fOMR\n", y10);
+                System.out.printf("YOUR CURRENCY IN TAKA IS: %.2f ৳\n", y10);
                 break;
 
             default:
